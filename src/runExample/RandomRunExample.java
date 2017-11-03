08c6e62a6b4b19d471da30720a6952fff5f5f323
Кирилл Малякин <malyakin-10@yandex.ru>
2017-11-03 13:33:43 +0300

    Класс полностью написан и протестирован

--- src/runExample/RandomRunExample.java
+++ src/runExample/RandomRunExample.java
@@ -3,6 +3,17 @@ package runExample;
 /**
  * Created by Кирилл on 03.11.2017.
  */
-public class RandomRunExample {
+public class RandomRunExample extends Thread {
+    public void run() {
+        System.out.println(Thread.currentThread().getName());
+    }
 
+    /**
+     * Создание и запуск 10 потоков
+     */
+    public static void example() {
+        for (int i = 0; i < 10; i++) {
+            (new RandomRunExample()).start();
+        }
+    }
 }
