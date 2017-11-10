5e6c89cf16254cfca35fb2741439d4118e2be87f
Кирилл Малякин <malyakin-10@yandex.ru>
2017-11-10 16:20:52 +0300

    Убран лишний код

--- src/catchUpGame/CatchUpGame.java
+++ src/catchUpGame/CatchUpGame.java
@@ -4,31 +4,13 @@ package catchUpGame;
  * Класс, демонстрирующий поведение потоков при изменении приоритета.
  * @author Малякин Кирилл. гр. 15-20
  */
-public class CatchUpGame extends Thread {
-    private final static int DELAY_TIME = 10; //Время для задержки каждого потока
-
-    int internalCounter = 0;
-
-    public void run() {
-        while (internalCounter < 100) {
-            if (internalCounter++ == 30) {
-                Thread.currentThread().setPriority(MIN_PRIORITY);
-            }
-            System.out.println(Thread.currentThread().getName() + " - " + internalCounter);
-            try {
-                Thread.currentThread().sleep(DELAY_TIME);
-            } catch (InterruptedException e) {
-
-            }
-        }
-        System.out.println(Thread.currentThread().getName() + " закончил работу");
-    }
+public class CatchUpGame {
 
     public static void main(String args[]) throws InterruptedException {
-        Thread firstThread = new CatchUpGame();
-        Thread secondThread = new SecondThread();
-        firstThread.setPriority(MAX_PRIORITY);
-        secondThread.setPriority(MIN_PRIORITY);
+        Thread firstThread = new SideThread();
+        Thread secondThread = new SideThread();
+        firstThread.setPriority(Thread.MAX_PRIORITY);
+        secondThread.setPriority(Thread.MIN_PRIORITY);
         firstThread.setName("Поток с максимальным начальным приоритетом");
         secondThread.setName("Поток с минимальным начальным приоритетом");
         firstThread.start();
@@ -36,13 +18,14 @@ public class CatchUpGame extends Thread {
 
     }
 
-    static class SecondThread extends Thread {
+    static class SideThread extends Thread {
         int internalCounter = 0;
+        private final static int DELAY_TIME = 10; //Время для задержки потока
 
         public void run() {
             while (internalCounter < 100) {
                 if (internalCounter++ == 30) {
-                    Thread.currentThread().setPriority(MAX_PRIORITY);
+                    Thread.currentThread().setPriority(Thread.currentThread().getPriority() == MAX_PRIORITY ? MIN_PRIORITY : MAX_PRIORITY);
                 }
                 System.out.println(Thread.currentThread().getName() + " - " + internalCounter);
                 try {
