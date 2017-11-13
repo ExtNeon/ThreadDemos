5438fca1da3646490429e313876d3a4a0066e663
Кирилл Малякин <malyakin-10@yandex.ru>
2017-11-13 10:56:01 +0300

    Класс написан и протестирован. Добавлены файлы.

--- src/parallelFileRead/Main.java
+++ src/parallelFileRead/Main.java
@@ -1,7 +1,34 @@
 package parallelFileRead;
 
+import java.io.*;
+
 /**
  * Created by Кирилл on 10.11.2017.
  */
 public class Main {
+    static final String FIRST_FILE = "src\\parallelFileRead\\files\\1.txt";
+    static final String SECOND_FILE = "src\\parallelFileRead\\files\\2.txt";
+    static final String RESULT_FILE = "src\\parallelFileRead\\files\\result.txt";
+
+    public static void main(String args[]) throws IOException {
+        ThreadedFileReader firstFile = new ThreadedFileReader(FIRST_FILE);
+        ThreadedFileReader secondFile = new ThreadedFileReader(SECOND_FILE);
+        try {
+            BufferedWriter resultFile = new BufferedWriter(new FileWriter(RESULT_FILE));
+            firstFile.join();
+            secondFile.join();
+            for (int i = 0; i < firstFile.getCountOfLines() || i < secondFile.getCountOfLines(); i++) {
+                if (i < firstFile.getCountOfLines()) {
+                    resultFile.write(firstFile.getLine(i) + '\n');
+                }
+                if (i < secondFile.getCountOfLines()) {
+                    resultFile.write(secondFile.getLine(i) + '\n');
+                }
+            }
+            resultFile.flush();
+        } catch (IOException e) {
+            throw new IOException("We have a small trouble:\n" + e.toString());
+        } catch (InterruptedException e) {}
+
+    }
 }
